ebfdaaecad893e342e518b3a94ce75a8347dd74e
hannah-bannanah <hannah-bannanah@users.noreply.github.com>
2022-04-07 12:01:28 +0200

    añadido swagger

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //import third party modules
 const express = require('express');
 const mongoose = require('mongoose');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 //import internal modules
 const { DBCONNECTION } = require('./util/db.config');
+const { SWAGGERSPEC } = require('./util/swagger.config');
 
 //import routes
 const adiestradorRouter = require('./routes/adiestrador.route');
@@ -11,9 +14,14 @@ const adiestradorRouter = require('./routes/adiestrador.route');
 //create app
 const app = express();
 
-// app.use(router);
+// middleware
 app.use(express.json()); //parser de json
 app.use(express.urlencoded({ extended: true })); //parser de objetos url.
+app.use(
+  '/swagger',
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJsDoc(SWAGGERSPEC))
+);
 
 // define routers
 app.use('/adiestradores', adiestradorRouter);
---
+++ routes/cliente.route.js
@@ -0,0 +1,21 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Cliente:
+ *       type: object
+ *       description: Un cliente
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: la direccion de email del cliente
+ *         password:
+ *           type: string
+ *           description: la password de usuario del adiestrador
+ *       required:
+ *        - email
+ *        - password
+ *       example:
+ *         email: "hannah@bannanah.com"
+ *         password: "secreta"
+ */
