953df46ef2522c6ca5a004052fe9dbe9263bad97
hannah-bannanah <hannah-bannanah@users.noreply.github.com>
2022-04-18 16:55:22 +0200

    arreglado bug

--- services/user.service.js
+++ services/user.service.js
@@ -98,7 +98,7 @@ exports.generateResetToken = async (email) => {
   const buffer = crypto.randomBytes(32);
   const token = buffer.toString('hex');
 
-  const user = await User.findOne({ email: email });
+  const user = await User.findOne({ email: email.toLowerCase() });
   if (!user) {
     const error = new Error('Usuario no encontrado');
     error.httpStatus = 404;
